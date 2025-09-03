dc0c890079e81dd82eec8a996a9400cd8704218c
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-09-03 17:27:46 +0530

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+    <h1 className='bold underline'>Hi</h1>      
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div> 
+    </>
+  )
+}
+
+export default App
